343feef0c4198b4c0b169c00dec255e9de171640
Maks <maks.kalinkin97@gmail.com>
2022-10-03 16:30:30 +0300

    сделал дз

---
+++ HomeWork/Program.cs
@@ -0,0 +1,70 @@
+﻿/*                                              Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine()!);
+int max = a;
+int min = b;
+
+if (b > max) 
+{
+    max = b;
+    Console.Write("max = ");
+Console.WriteLine(max);
+}
+else
+{
+    Console.Write("min = ");
+Console.WriteLine(min);
+}*/
+
+/*                                              Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine("ВВедите третье число");
+int c = Convert.ToInt32(Console.ReadLine()!);
+int max = a;
+
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+
+Console.Write("max = ");
+Console.WriteLine(max);;*/
+
+                                             /* Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Clear();
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine()!);
+
+
+if (a % 2 == 0) 
+{
+    Console.WriteLine("Четное  ");
+}
+
+else
+{
+    Console.WriteLine("Нечетное ");
+}*/
+/*                                              Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+/*Console.Clear();
+Console.WriteLine("Введите N ");
+int n = int.Parse(Console.ReadLine()!);
+    
+    for (int i = 1; i <= n; i++)
+    if (i % 2 == 0)
+    {
+    Console.WriteLine(i);
+    }*/
